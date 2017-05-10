fd5668268c8375cbcdfc62becc046bb26d3f809d
Budi Irawan <deerawan@gmail.com>
2017-05-11 09:34:42 +1000

    rebuild

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,12 @@
-export * from './metric';
+import * as kue from 'kue';
+export interface Options {
+    queue: kue.Queue;
+    jobName: string;
+    prefixMetricName?: string;
+    interval?: number;
+}
+declare const KueProm: (opts: Options) => {
+    run: () => void;
+    stop: () => void;
+};
+export default KueProm;
