766587c1f93aabd89777977423e4f1dfec9fb758
Robbie Trencheny <me@robbiet.us>
2020-05-28 19:37:04 -0700

    Update for prom-client v12

--- src/index.ts
+++ src/index.ts
@@ -21,59 +21,63 @@ export function init(opts: Options) {
   const completedMetric = new promClient.Gauge({
     name: completedMetricName,
     help: 'Number of completed jobs',
-    defaultLabels: [QUEUE_NAME_LABEL],
+    labelNames: [QUEUE_NAME_LABEL],
   });
 
   const failedMetric = new promClient.Gauge({
     name: failedMetricName,
     help: 'Number of failed jobs',
-    defaultLabels: [QUEUE_NAME_LABEL],
+    labelNames: [QUEUE_NAME_LABEL],
   });
 
   const delayedMetric = new promClient.Gauge({
     name: delayedMetricName,
     help: 'Number of delayed jobs',
-    defaultLabels: [QUEUE_NAME_LABEL],
+    labelNames: [QUEUE_NAME_LABEL],
   });
 
   const activeMetric = new promClient.Gauge({
     name: activeMetricName,
     help: 'Number of active jobs',
-    defaultLabels: [QUEUE_NAME_LABEL],
+    labelNames: [QUEUE_NAME_LABEL],
   });
 
   const waitingMetric = new promClient.Gauge({
     name: waitingMetricName,
     help: 'Number of waiting jobs',
-    defaultLabels: [QUEUE_NAME_LABEL],
+    labelNames: [QUEUE_NAME_LABEL],
   });
 
   const durationMetric = new promClient.Summary({
     name: durationMetricName,
     help: 'Duration of jobs',
-    defaultLabels: [QUEUE_NAME_LABEL],
+    labelNames: [QUEUE_NAME_LABEL],
   });
 
   function start(queue: bull.Queue) {
     let metricInterval: any;
+
+    const labels = {
+      [QUEUE_NAME_LABEL]: queue.name
+    }
 
     queue.on('completed', (job) => {
       if (!job.finishedOn) {
         return;
       }
       const duration = job.finishedOn - job.processedOn!;
-      durationMetric.labels((queue as any).name).observe(duration);
+      durationMetric.observe(labels, duration);
     });
 
     metricInterval = setInterval(() => {
       queue
         .getJobCounts()
         .then(({ completed, failed, delayed, active, waiting }) => {
-          completedMetric.labels((queue as any).name).set(completed || 0);
-          failedMetric.labels((queue as any).name).set(failed || 0);
-          delayedMetric.labels((queue as any).name).set(delayed || 0);
-          activeMetric.labels((queue as any).name).set(active || 0);
-          waitingMetric.labels((queue as any).name).set(waiting || 0);
+          completedMetric.set(labels, (completed || 0));
+          failedMetric.set(labels, (failed || 0));
+          delayedMetric.set(labels, (delayed || 0));
+          activeMetric.set(labels, (active || 0));
+          waitingMetric.set(labels, (waiting || 0));
         });
     }, interval);
     return {
